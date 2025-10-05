3a3c85d08acfef0c25f10690ebaacabfddfc34ca
Muhammad Ubaid Alam <160329146+Ubaidalam217@users.noreply.github.com>
2025-10-05 23:16:16 +0500

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,30 @@
+// function greet(name, greetText){
+   
+//     console.log( greetText + " " , name)
+//     console.log( name + " is a good boy")
+// }
+// let name1= "ubaid";
+// let name2= "uzair";
+// let name3= "umair";
+// let greetText = "Godd Morning";
+// greet(name1 , greetText);
+// greet(name2, greetText);
+// greet(name3 , greetText);
+
+sum
+function sum(a,b,c){
+    d = a + b + c;
+    return d;
+}
+console.log(sum(1,2,3))
+
+function maximum(a , b){
+if(a > b){
+    return a;
+     }
+    else{
+        return b
+    }
+
+}
+console.log(maximum(112,110))
